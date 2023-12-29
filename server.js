1e8c475d4418d2fd84dac01b92df79a4b3337cd2
Julio Santana <juliosantana3009@gmail.com>
2023-12-29 11:05:14 -0300

    install cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const cors = require('cors')
 
 const connectDB = require('./config/db')
 
