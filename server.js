fc21fa454e2425050414aaba447afb3f6db72813
Julio Santana <juliosantana3009@gmail.com>
2024-01-09 16:32:09 -0300

    prueba

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const cors = require('cors')
 
-const connectDB = require('./config/db')
+//const connectDB = require('./config/db')
 
-connectDB()
+//connectDB()
 
 const app = express()
 
@@ -13,7 +13,31 @@ app.use(express.json())
 app.use( express.urlencoded( { extended: false } ) )
 app.use(cors())
 
-app.use('/api', require('./routes/userRoute'))
-app.use( '/api', require( './routes/loginRoute' ) )
+//app.use('/api', require('./routes/userRoute'))
+//app.use( '/api', require( './routes/loginRoute' ) )
 
-app.listen(port, () => console.log(`Server running on port: ${port}`))
+//app.listen(port, () => console.log(`Server running on port: ${port}`))
+
+// Imports the Google Cloud client library
+
+const {IAMCredentialsClient} = require('@google-cloud/iam-credentials');
+
+// TODO(developer): replace with your prefered project values.
+// The service account must be granted the roles/iam.serviceAccountTokenCreator role
+// const serviceAccount = 'ACCOUNT_EMAIL_OR_UNIQUEID'
+// const scopes = 'my-scopes', e.g., 'https://www.googleapis.com/auth/iam'
+
+// Creates a client
+const client = new IAMCredentialsClient();
+
+const serviceAccount = "jsantana@soaint.com"
+const scopes = "email"
+
+async function generateAccessToken() {
+  const [token] = await client.generateAccessToken({
+    name: `projects/-/serviceAccounts/${serviceAccount}`,
+    scope: [scopes],
+  });
+  console.info(token);
+}
+generateAccessToken();
