45d105c35c949f6af06f4da31beeacae6321dd3f
emilio <jebv.informatico@gmail.com>
2024-01-10 11:08:25 -0500

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const { IAMCredentialsClient } = require("@google-cloud/iam-credentials");
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 const cors = require("cors");
 
 //const connectDB = require('./config/db')
